0fb63bd8db9214bb0106096797a5dd3366c77419
CCDevX <cavaleri.cristina.dev@gmail.com>
2025-04-12 22:42:34 +0200

    feat: First deliverable completed and ready for client review

---
+++ src/pages/home/home.js
@@ -0,0 +1,23 @@
+import "./home.scss";
+
+const initHomePage = () => {
+  const logoUrl = new URL("../../assets/img/logo.png", import.meta.url).href;
+  const localUrl = new URL("../../assets/img/espace_priv.jpg", import.meta.url)
+    .href;
+  const groceryUrl = new URL(
+    "../../assets/img/epicerie_fine.jpg",
+    import.meta.url
+  ).href;
+  const terraceUrl = new URL("../../assets/img/terrasse.jpg", import.meta.url)
+    .href;
+  const imgLogo = document.querySelector("#logo-home-page");
+  const imgLocal = document.querySelector("#local");
+  const imgGrocery = document.querySelector("#grocery");
+  const imgTerrace = document.querySelector("#terrace");
+  imgLogo.src = logoUrl;
+  imgLocal.src = localUrl;
+  imgGrocery.src = groceryUrl;
+  imgTerrace.src = terraceUrl;
+};
+
+initHomePage();
